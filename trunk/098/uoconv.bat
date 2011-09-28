3a3a40e6e5cbad84b6a08324ef5565040714d4be
natalia_buenota@hotmail.com <natalia_buenota@hotmail.com@6fc97285-06d1-f6fa-dd5c-d3bec1f78033>
2011-09-28 16:10:07 +0000

    the WoD scripts for POL 098

---
+++ trunk/098/uoconv.bat
@@ -0,0 +1,30 @@
+uoconvert multis
+copy multis.cfg config
+
+uoconvert tiles
+copy tiles.cfg config
+
+uoconvert landtiles
+copy landtiles.cfg config
+
+rem Mondain's Legacy use "width=7168" here
+uoconvert map     realm=britannia mapid=0 usedif=1 width=7168 height=4096
+uoconvert statics realm=britannia
+uoconvert maptile realm=britannia
+
+rem Mondain's Legacy use "width=7168" here
+uoconvert map     realm=britannia_alt mapid=1 usedif=1 width=7168 height=4096
+uoconvert statics realm=britannia_alt
+uoconvert maptile realm=britannia_alt
+
+uoconvert map     realm=ilshenar mapid=2 usedif=1 width=2304 height=1600
+uoconvert statics realm=ilshenar
+uoconvert maptile realm=ilshenar
+
+uoconvert map     realm=malas mapid=3 usedif=1 width=2560 height=2048
+uoconvert statics realm=malas
+uoconvert maptile realm=malas
+
+uoconvert map     realm=tokuno mapid=4 usedif=1 width=1448 height=1448
+uoconvert statics realm=tokuno
+uoconvert maptile realm=tokuno
