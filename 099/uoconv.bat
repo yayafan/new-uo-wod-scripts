612bf434178deb846bdccad8db9d30f69fccf9a3
natalia_buenota <natalia_buenota@hotmail.com>
2011-11-25 00:54:29 +0000

    - Configured again for High Seas client. - Increased the Secure Container capacity. - Some fixes to the .grantpriv command. - We now get sounds when using a moongate. - Added script for unloading scripts without restarting the server. - Added a script for upgrading all accounts. - Added Mysticism and Bard Masteries spellbooks and scrolls. The spells need to be scripted now. - Started work on the Paladin and Necromancy spellbooks

--- 099/uoconv.bat
+++ 099/uoconv.bat
@@ -1,31 +1,31 @@
-uoconvert multis
-copy multis.cfg config
+rem uoconvert multis
+rem copy multis.cfg config
 
-uoconvert tiles
-copy tiles.cfg config
+rem uoconvert tiles
+rem copy tiles.cfg config
 
-uoconvert landtiles
-copy landtiles.cfg config
+rem uoconvert landtiles
+rem copy landtiles.cfg config
 
-uoconvert map     realm=britannia mapid=0 usedif=1 width=6144 height=4096
+uoconvert map     realm=britannia mapid=0 usedif=1 width=7168 height=4096
 uoconvert statics realm=britannia
 uoconvert maptile realm=britannia
 
-uoconvert map     realm=britannia_alt mapid=1 usedif=1 width=6144 height=4096
-uoconvert statics realm=britannia_alt
-uoconvert maptile realm=britannia_alt
+rem uoconvert map     realm=britannia_alt mapid=1 usedif=1 width=7168 height=4096
+rem uoconvert statics realm=britannia_alt
+rem uoconvert maptile realm=britannia_alt
 
-uoconvert map     realm=ilshenar mapid=2 usedif=1 width=2304 height=1600
-uoconvert statics realm=ilshenar
-uoconvert maptile realm=ilshenar
+rem uoconvert map     realm=ilshenar mapid=2 usedif=1 width=2304 height=1600
+rem uoconvert statics realm=ilshenar
+rem uoconvert maptile realm=ilshenar
 
-uoconvert map     realm=malas mapid=3 usedif=1 width=2560 height=2048
-uoconvert statics realm=malas
-uoconvert maptile realm=malas
+rem uoconvert map     realm=malas mapid=3 usedif=1 width=2560 height=2048
+rem uoconvert statics realm=malas
+rem uoconvert maptile realm=malas
 
-uoconvert map     realm=tokuno mapid=4 usedif=1 width=1448 height=1448
-uoconvert statics realm=tokuno
-uoconvert maptile realm=tokuno
+rem uoconvert map     realm=tokuno mapid=4 usedif=1 width=1448 height=1448
+rem uoconvert statics realm=tokuno
+rem uoconvert maptile realm=tokuno
 
 rem uoconvert map     realm=termur mapid=5 usedif=1 width=1280 height=4096
 rem uoconvert statics realm=termur
