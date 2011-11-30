50fa8daa3cd4ebcf743fdde5cf6a05ef515e5855
natalia_buenota@hotmail.com <natalia_buenota@hotmail.com@6fc97285-06d1-f6fa-dd5c-d3bec1f78033>
2011-11-30 17:00:08 +0000

    - Added Winged Helmet, Dragon Armor, Leaf Armor, Hide Armor, Woodland Armor, and all Elven Headgear. - No skillchecks were being made for and no reagents were being consumed for Magery spells when cast from the spellbook or macros. Fixed. - Old routines for checking if a spellbook contains certain spell no longer work. Fixed three such instances so far, need to look for more. - Added support for all the new mounts up to High Seas expansion.

--- trunk/099/uoconv.bat
+++ trunk/099/uoconv.bat
@@ -11,22 +11,22 @@ uoconvert map     realm=britannia mapid=0 usedif=1 width=7168 height=4096
 uoconvert statics realm=britannia
 uoconvert maptile realm=britannia
 
-rem uoconvert map     realm=britannia_alt mapid=1 usedif=1 width=7168 height=4096
-rem uoconvert statics realm=britannia_alt
-rem uoconvert maptile realm=britannia_alt
+uoconvert map     realm=britannia_alt mapid=1 usedif=1 width=7168 height=4096
+uoconvert statics realm=britannia_alt
+uoconvert maptile realm=britannia_alt
 
-rem uoconvert map     realm=ilshenar mapid=2 usedif=1 width=2304 height=1600
-rem uoconvert statics realm=ilshenar
-rem uoconvert maptile realm=ilshenar
+uoconvert map     realm=ilshenar mapid=2 usedif=1 width=2304 height=1600
+uoconvert statics realm=ilshenar
+uoconvert maptile realm=ilshenar
 
-rem uoconvert map     realm=malas mapid=3 usedif=1 width=2560 height=2048
-rem uoconvert statics realm=malas
-rem uoconvert maptile realm=malas
+uoconvert map     realm=malas mapid=3 usedif=1 width=2560 height=2048
+uoconvert statics realm=malas
+uoconvert maptile realm=malas
 
-rem uoconvert map     realm=tokuno mapid=4 usedif=1 width=1448 height=1448
-rem uoconvert statics realm=tokuno
-rem uoconvert maptile realm=tokuno
+uoconvert map     realm=tokuno mapid=4 usedif=1 width=1448 height=1448
+uoconvert statics realm=tokuno
+uoconvert maptile realm=tokuno
 
-rem uoconvert map     realm=termur mapid=5 usedif=1 width=1280 height=4096
-rem uoconvert statics realm=termur
-rem uoconvert maptile realm=termur
+uoconvert map     realm=termur mapid=5 usedif=1 width=1280 height=4096
+uoconvert statics realm=termur
+uoconvert maptile realm=termur
